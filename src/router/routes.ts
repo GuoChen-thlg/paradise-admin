a4e5c5498e2391940d0281bd7f9e49c9b9845cf8
天火流光 <gc.thlg@gmail.com>
2021-06-27 13:39:01 +0800

    updata:Reincarnation
    Signed-off-by: 天火流光 <gc.thlg@gmail.com>

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,348 +5,264 @@ import Login from '@/views/Login.vue'
 import { AUTHORITY } from '@/enum'
 
 export const routes: Array<RouteRecordRaw> = [
-	{
-		/*  */
-		path: '/',
-		redirect: () => {
-			if (store.state.user.login_statu === true) {
-				return '/home'
-			} else {
-				return '/login'
-			}
-		},
-	},
-	{
-		/* 基础页面  */
-
-		path: '/about',
-		name: 'About',
-		component: () =>
-			import(/* webpackChunkName: "about" */ '@/views/About.vue'),
-		meta: {
-			showAside: false,
-			showHeader: false,
-			showFooter: false,
-			showTabNav: false,
-			title: '关于',
-			authority: [],
-			verifyLogin: false,
-		},
-	},
-	{
-		/* 登录页面 登录后不可见 */
-		path: '/login',
-		name: 'Login',
-		component: Login,
-		meta: {
-			showAside: false,
-			showHeader: false,
-			showFooter: false,
-			showTabNav: false,
-			authority: [],
-			title: '南天门登记处',
-			verifyLogin: false,
-		},
-	},
-	{
-		/* 未找到 */
-		path: '/404',
-		name: 'NotFound',
-		component: () =>
-			import(/* webpackChunkName: "not-fount" */ '@/views/NotFount.vue'),
-		meta: {
-			showAside: false,
-			showHeader: false,
-			showFooter: false,
-			showTabNav: false,
-			authority: [],
-			verifyLogin: false,
-			title: '404',
-		},
-	},
-	{
-		/*  */
-		path: '/:pathMatch(.*)*',
-		redirect: { name: 'NotFound' },
-	},
-	/* *********************************************************************** */
-	{
-		/* 首页 */
-		path: '/home',
-		name: 'Home',
-		component: () =>
-			import(/* webpackChunkName: "about" */ '@/views/Home.vue'),
-		meta: {
-			showAside: true,
-			showHeader: true,
-			showFooter: true,
-			showTabNav: true,
-			title: '管理系统',
-			authority: [],
-			verifyLogin: true,
-		},
-	},
-	{
-		/* 人员列表 不可增删改同等级及以上人员信息 */
-		path: '/personnel',
-		name: 'Personnel',
-		component: () =>
-			import(
-				/* webpackChunkName: "personnel" */ '@/views/personnel-bureau/Personnel.vue'
-			),
-		meta: {
-			showAside: true,
-			showHeader: true,
-			showFooter: true,
-			showTabNav: true,
-			authority: [AUTHORITY.PERSONNEL_R],
-			title: '人员信息',
-			verifyLogin: true,
-		},
-		children: [
 			{
 				/*  */
-				path: ':id(\\d+)',
+				path: '/',
+				redirect: () => {
+					if (store.state.user.login_statu === true) {
+						return '/home'
+					} else {
+						return '/login'
+					}
+				},
+			},
+			{
+				/* 基础页面  */
+
+				path: '/about',
+				name: 'About',
+				component: () =>
+					import(/* webpackChunkName: "about" */ '@/views/About.vue'),
+				meta: {
+					showAside: false,
+					showHeader: false,
+					showFooter: false,
+					showTabNav: false,
+					title: '关于',
+					authority: [],
+					verifyLogin: false,
+				},
+			},
+			{
+				/* 登录页面 登录后不可见 */
+				path: '/login',
+				name: 'Login',
+				component: Login,
+				meta: {
+					showAside: false,
+					showHeader: false,
+					showFooter: false,
+					showTabNav: false,
+					authority: [],
+					title: '南天门登记处',
+					verifyLogin: false,
+				},
+			},
+			{
+				/* 未找到 */
+				path: '/404',
+				name: 'NotFound',
+				component: () =>
+					import(
+						/* webpackChunkName: "not-fount" */ '@/views/NotFount.vue'
+					),
+				meta: {
+					showAside: false,
+					showHeader: false,
+					showFooter: false,
+					showTabNav: false,
+					authority: [],
+					verifyLogin: false,
+					title: '404',
+				},
+			},
+			{
+				/*  */
+				path: '/:pathMatch(.*)*',
+				redirect: { name: 'NotFound' },
+			},
+			/* *********************************************************************** */
+			{
+				/* 首页 */
+				path: '/home',
+				name: 'Home',
+				component: () =>
+					import(/* webpackChunkName: "about" */ '@/views/Home.vue'),
+				meta: {
+					showAside: true,
+					showHeader: true,
+					showFooter: true,
+					showTabNav: true,
+					title: '管理系统',
+					authority: [],
+					verifyLogin: true,
+				},
+			},
+			{
+				/* 人员列表 不可增删改同等级及以上人员信息 */
+				path: '/personnel',
 				name: 'Personnel',
 				component: () =>
 					import(
-						/* webpackChunkName: "edit-personnel" */ '@/views/personnel-bureau/EditPersonnel.vue'
-					),
-				props: {},
-				meta: {
-					showAside: true,
-					showHeader: true,
-					showFooter: true,
-					showTabNav: true,
-					authority: [
-						AUTHORITY.PERSONNEL_C,
-						AUTHORITY.PERSONNEL_R,
-						AUTHORITY.PERSONNEL_U,
-						AUTHORITY.PERSONNEL_D,
-					],
-					title: '编辑人员信息',
-					verifyLogin: true,
-				},
-			},
-			{
-				/*  */
-				path: 'create',
-				name: 'Addnew',
-				component: () =>
-					import(
-						/* webpackChunkName: "add-new" */ '@/views/personnel-bureau/AddNew.vue'
-					),
-				props: {},
-				meta: {
-					showAside: true,
-					showHeader: true,
-					showFooter: true,
-					showTabNav: true,
-					authority: [AUTHORITY.PERSONNEL_C],
-					title: '添加人员',
-					verifyLogin: true,
-				},
-			},
-		],
-	},
-	{
-		path: '/setting',
-		name: 'Setting',
-		component: () =>
-			import(
-				/* webpackChunkName: "router-view" */ '@/views/router-view.vue'
-			),
-		children: [
-			{
-				path: 'authority',
-				name: 'Authority',
-				component: () =>
-					import(
-						/* webpackChunkName: "authority" */ '@/views/setting/Authority.vue'
-					),
-			},
-			{
-				path: 'edit-role/:role?',
-				name: 'EditRole',
-				component: () =>
-					import(
-						/* webpackChunkName: "edit-role" */ '@/views/setting/EditRole.vue'
-					),
-			},
-		],
-	},
-	{
-		/*  */
-		path: '/product',
-		component: () =>
-			import(
-				/* webpackChunkName: "router-view" */ '@/views/router-view.vue'
-			),
-		children: [
-			{
-				/*  */
-				path: '',
-				name: 'Product',
-				component: () =>
-					import(
-						/* webpackChunkName: "product" */ '@/views/product/Product.vue'
+						/* webpackChunkName: "personnel" */ '@/views/personnel-bureau/Personnel.vue'
 					),
 				meta: {
 					showAside: true,
 					showHeader: true,
 					showFooter: true,
 					showTabNav: true,
-					authority: ['a'],
-					title: '兜率宫',
+					authority: [AUTHORITY.PERSONNEL_R],
+					title: '人员信息',
 					verifyLogin: true,
 				},
+				children: [
+					{
+						/*  */
+						path: ':id(\\d+)',
+						name: 'Personnel',
+						component: () =>
+							import(
+								/* webpackChunkName: "edit-personnel" */ '@/views/personnel-bureau/EditPersonnel.vue'
+							),
+						props: {},
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [
+								AUTHORITY.PERSONNEL_C,
+								AUTHORITY.PERSONNEL_R,
+								AUTHORITY.PERSONNEL_U,
+								AUTHORITY.PERSONNEL_D,
+							],
+							title: '编辑人员信息',
+							verifyLogin: true,
+						},
+					},
+					{
+						/*  */
+						path: 'create',
+						name: 'Addnew',
+						component: () =>
+							import(
+								/* webpackChunkName: "add-new" */ '@/views/personnel-bureau/AddNew.vue'
+							),
+						props: {},
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [AUTHORITY.PERSONNEL_C],
+							title: '添加人员',
+							verifyLogin: true,
+						},
+					},
+				],
+			},
+			{
+				path: '/setting',
+				name: 'Setting',
+				component: () =>
+					import(
+						/* webpackChunkName: "router-view" */ '@/views/router-view.vue'
+					),
+				children: [
+					{
+						path: 'authority',
+						name: 'Authority',
+						component: () =>
+							import(
+								/* webpackChunkName: "authority" */ '@/views/setting/Authority.vue'
+							),
+					},
+					{
+						path: 'edit-role/:role?',
+						name: 'EditRole',
+						component: () =>
+							import(
+								/* webpackChunkName: "edit-role" */ '@/views/setting/EditRole.vue'
+							),
+					},
+				],
 			},
 			{
 				/*  */
-				path: 'details/:vendor_id/:product_id',
-				name: 'Details',
+				path: '/product',
 				component: () =>
 					import(
-						/* webpackChunkName: "details" */ '@/views/product/Details.vue'
+						/* webpackChunkName: "router-view" */ '@/views/router-view.vue'
 					),
-				meta: {
-					showAside: true,
-					showHeader: true,
-					showFooter: true,
-					showTabNav: true,
-					authority: [],
-					title: '产品编辑',
-					verifyLogin: true,
-				},
+				children: [
+					{
+						/*  */
+						path: '',
+						name: 'Product',
+						component: () =>
+							import(
+								/* webpackChunkName: "product" */ '@/views/product/Product.vue'
+							),
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: ['a'],
+							title: '兜率宫',
+							verifyLogin: true,
+						},
+					},
+					{
+						/*  */
+						path: 'details/:vendor_id/:product_id',
+						name: 'Details',
+						component: () =>
+							import(
+								/* webpackChunkName: "details" */ '@/views/product/Details.vue'
+							),
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [],
+							title: '产品编辑',
+							verifyLogin: true,
+						},
+					},
+					{
+						/*  */
+						path: 'edit-product/:vendor_id/:product_id',
+						name: 'EditProduct',
+						component: () =>
+							import(
+								/* webpackChunkName: "edit-product" */ '@/views/product/EditProduct.vue'
+							),
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [],
+							title: '产品编辑',
+							verifyLogin: true,
+						},
+					},
+				],
 			},
 			{
 				/*  */
-				path: 'edit-product/:vendor_id/:product_id',
-				name: 'EditProduct',
+				path: '/stock',
+				name: 'Stock',
 				component: () =>
-					import(
-						/* webpackChunkName: "edit-product" */ '@/views/product/EditProduct.vue'
-					),
-				meta: {
-					showAside: true,
-					showHeader: true,
-					showFooter: true,
-					showTabNav: true,
-					authority: [],
-					title: '产品编辑',
-					verifyLogin: true,
-				},
-			},
-		],
-	},
-	{
-		/*  */
-		path: '/stock',
-		name: 'Stock',
-		component: () =>
-			import(/* webpackChunkName: "stock" */ '@/views/Stock.vue'),
-		meta: {
-			showAside: true,
-			showHeader: true,
-			showFooter: true,
-			showTabNav: true,
-			authority: [],
-			title: '股价',
-			verifyLogin: true,
-		},
-	},
-	{
-		/*  */
-		path: '/hell',
-		component: () =>
-			import(
-				/* webpackChunkName: "router-view" */ '@/views/router-view.vue'
-			),
-		meta: {
-			showAside: true,
-			showHeader: true,
-			showFooter: true,
-			showTabNav: true,
-			authority: [],
-			title: '幽冥地府',
-			verifyLogin: true,
-		},
-		children: [
-			{
-				/*  */
-				path: '',
-				name: 'Hell',
-				component: () =>
-					import(
-						/* webpackChunkName: "hell" */ '@/views/hell/Hell.vue'
-					),
+					import(/* webpackChunkName: "stock" */ '@/views/Stock.vue'),
 				meta: {
 					showAside: true,
 					showHeader: true,
 					showFooter: true,
 					showTabNav: true,
 					authority: [],
-					title: '幽冥地府',
+					title: '股价',
 					verifyLogin: true,
 				},
 			},
 			{
 				/*  */
-				path: 'danger-spot',
-				name: 'DangerSpot',
+				path: '/hell',
 				component: () =>
 					import(
-						/* webpackChunkName: "danger-spot" */ '@/views/hell/DangerSpot.vue'
-					),
-				meta: {
-					showAside: true,
-					showHeader: true,
-					showFooter: true,
-					showTabNav: true,
-					authority: [],
-					title: '鬼门关',
-					verifyLogin: true,
-				},
-			},
-		],
-	},
-	{
-		path: '/share',
-		component: () =>
-			import(/* webpackChunkName: "share" */ '@/views/share/Share.vue'),
-		meta: {
-			showAside: true,
-			showHeader: true,
-			showFooter: true,
-			showTabNav: true,
-			authority: [],
-			title: '分享',
-			verifyLogin: true,
-		},
-	},
-	{
-		/*  */
-		path: '/world',
-		component: () =>
-			import(
-				/* webpackChunkName: "router-view" */ '@/views/router-view.vue'
-			),
-		meta: {
-			showAside: true,
-			showHeader: true,
-			showFooter: true,
-			showTabNav: true,
-			authority: [],
-			title: '人界',
-			verifyLogin: true,
-		},
-		children: [
-			{
-				/*  */
-				path: '',
-				component: () =>
-					import(
-						/* webpackChunkName: "integrate" */ '@/views/world/Integrate.vue'
+						/* webpackChunkName: "router-view" */ '@/views/router-view.vue'
 					),
 				meta: {
 					showAside: true,
@@ -357,13 +273,68 @@ export const routes: Array<RouteRecordRaw> = [
 					title: '幽冥地府',
 					verifyLogin: true,
 				},
+				children: [
+					{
+						/*  */
+						path: '',
+						name: 'Hell',
+						component: () =>
+							import(
+								/* webpackChunkName: "hell" */ '@/views/hell/Hell.vue'
+							),
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [],
+							title: '幽冥地府',
+							verifyLogin: true,
+						},
+					},
+					{
+						/*  */
+						path: 'danger-spot',
+						name: 'DangerSpot',
+						component: () =>
+							import(
+								/* webpackChunkName: "danger-spot" */ '@/views/hell/DangerSpot.vue'
+							),
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [],
+							title: '鬼门关',
+							verifyLogin: true,
+						},
+					},
+					{
+						/*  */
+						path: 'reincarnation',
+						name: 'Reincarnation',
+						component: () =>
+							import(
+								/* webpackChunkName: "reincarnation" */ '@/views/hell/Reincarnation.vue'
+							),
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [],
+							title: '六道轮回',
+							verifyLogin: true,
+						},
+					},
+				],
 			},
 			{
-				/*  */
-				path: 'territory/:continent',
+				path: '/share',
 				component: () =>
 					import(
-						/* webpackChunkName: "territory" */ '@/views/world/Territory.vue'
+						/* webpackChunkName: "share" */ '@/views/share/Share.vue'
 					),
 				meta: {
 					showAside: true,
@@ -371,10 +342,61 @@ export const routes: Array<RouteRecordRaw> = [
 					showFooter: true,
 					showTabNav: true,
 					authority: [],
-					title: '幽冥地府',
+					title: '分享',
 					verifyLogin: true,
 				},
 			},
-		],
-	},
-]
+			{
+				/*  */
+				path: '/world',
+				component: () =>
+					import(
+						/* webpackChunkName: "router-view" */ '@/views/router-view.vue'
+					),
+				meta: {
+					showAside: true,
+					showHeader: true,
+					showFooter: true,
+					showTabNav: true,
+					authority: [],
+					title: '人界',
+					verifyLogin: true,
+				},
+				children: [
+					{
+						/*  */
+						path: '',
+						component: () =>
+							import(
+								/* webpackChunkName: "integrate" */ '@/views/world/Integrate.vue'
+							),
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [],
+							title: '幽冥地府',
+							verifyLogin: true,
+						},
+					},
+					{
+						/*  */
+						path: 'territory/:continent',
+						component: () =>
+							import(
+								/* webpackChunkName: "territory" */ '@/views/world/Territory.vue'
+							),
+						meta: {
+							showAside: true,
+							showHeader: true,
+							showFooter: true,
+							showTabNav: true,
+							authority: [],
+							title: '幽冥地府',
+							verifyLogin: true,
+						},
+					},
+				],
+			},
+		]
---
+++ src/views/hell/Reincarnation.vue
@@ -0,0 +1,265 @@
+<template>
+  <el-main class="reincarnation">
+    <div class="container">
+      <div id="wheel-of-fate"></div>
+    </div>
+    <h1>{{ result }}</h1>
+    <!-- 善 <el-input /> 恶 <el-input /> -->
+    <el-button @click="handleStart"> Start </el-button>
+  </el-main>
+</template>
+<script lang="ts">
+import Konva from 'konva'
+import { Group } from 'konva/lib/Group'
+import { Shape } from 'konva/lib/Shape'
+import { Animation } from 'konva/lib/Animation'
+import { Text } from 'konva/lib/shapes/Text'
+import { Stage } from 'konva/lib/Stage'
+import { defineComponent, onMounted, ref } from 'vue'
+import { IFrame } from 'konva/lib/types'
+import { Wedge } from 'konva/lib/shapes/Wedge'
+
+export default defineComponent({
+  name: 'Reincarnation',
+  setup() {
+    const result = ref('')
+
+    const six = ['天道', '人道', '修罗道', '地狱道', '饿鬼道', '畜生道']
+
+    let angularVelocity = 20
+    let lastRotation = 0
+    const angularVelocities: number[] = []
+
+    const angle = (2 * Math.PI) / 6
+
+    let controlled = false
+    const angularFriction = 0.2
+
+    let target: Wedge
+    let activeWedge: Shape
+    let stage: Stage
+
+    let wheel: Group //
+    let pointer: Wedge // 指针
+    let finished = false // 完成
+    let anim: Animation
+
+    Konva.angleDeg = false
+
+    // 平均角速度
+    const handleAverageAngularVelocity = () =>
+      angularVelocities.length === 0
+        ? 0
+        : angularVelocities.reduce((count, s) => count + s, 0) /
+          angularVelocities.length
+
+    function animate(frame: IFrame | undefined) {
+      // handle wheel spin
+      const angularVelocityChange =
+        (angularVelocity * (frame as IFrame).timeDiff * (1 - angularFriction)) /
+        1000
+      angularVelocity -= angularVelocityChange
+
+      // 指针当前选中的图形
+      const shape = stage.getIntersection({
+        x: stage.width() / 2,
+        y: 100,
+      })
+
+      if (controlled) {
+        if (angularVelocities.length > 10) {
+          angularVelocities.shift()
+        }
+        angularVelocities.push(
+          ((wheel.rotation() - lastRotation) * 1000) /
+            (frame as IFrame).timeDiff
+        )
+      } else {
+        const diff = ((frame as IFrame).timeDiff * angularVelocity) / 1000
+        if (diff > 0.0001) {
+          wheel.rotate(diff)
+        } else if (!finished && !controlled) {
+          if (shape) {
+            const text = (
+              shape.getParent().findOne('Text') as unknown as Text
+            ).text()
+            const price = text.split('\n').join('')
+            anim?.stop()
+            result.value = price
+            console.log('------------*------------' + price)
+          }
+          finished = true
+        }
+      }
+      lastRotation = wheel.rotation()
+
+      if (shape) {
+        if (shape && (!activeWedge || shape._id !== activeWedge._id)) {
+          pointer.y(20)
+          new Konva.Tween({
+            node: pointer,
+            duration: 0.3,
+            y: 30,
+            easing: Konva.Easings.ElasticEaseOut,
+          }).play()
+
+          activeWedge?.fillPriority('radial-gradient')
+
+          shape.fillPriority('fill')
+          activeWedge = shape
+        }
+      }
+    }
+
+    function init() {
+      // 初始化
+      stage = new Konva.Stage({
+        container: 'wheel-of-fate',
+        width: 400,
+        height: 400,
+      })
+      const layer = new Konva.Layer()
+      //   轮盘
+      wheel = new Konva.Group({
+        x: stage.width() / 2,
+        y: stage.height() / 2,
+      })
+      six.forEach((t, i) => {
+        //   组
+        const group_wedge = new Konva.Group({
+          rotation: (2 * i * Math.PI) / 6,
+        })
+        //  背景
+        const wedgeBackground = new Konva.Wedge({
+          radius: 200,
+          angle: angle,
+          fillRadialGradientStartPoint: { x: 0, y: 0 },
+          fillRadialGradientStartRadius: 0,
+          fillRadialGradientEndPoint: { x: 0, y: 0 },
+          fillRadialGradientEndRadius: 200,
+          fillRadialGradientColorStops: [0, '#f4f6f2', 1, '#5f68f6'],
+          fill: '#64e9f8',
+          fillPriority: 'radial-gradient',
+          stroke: '#ccc',
+          strokeWidth: 2,
+        })
+
+        group_wedge.add(wedgeBackground)
+
+        // 文字
+        const text = new Konva.Text({
+          text: t,
+          fontFamily: 'Calibri',
+          fontSize: 30,
+          fill: 'white',
+          align: 'center',
+          stroke: 'yellow',
+          strokeWidth: 1,
+          rotation: (Math.PI + angle) / 2,
+          x: (stage.width() / 8) * 3,
+          y: stage.width() / 8,
+          listening: false,
+        })
+
+        group_wedge.add(text)
+        group_wedge.id(`${group_wedge.rotation()}`)
+        wheel.add(group_wedge)
+      })
+      //   console.log(layer)
+
+      //   指针
+      pointer = new Konva.Wedge({
+        fillRadialGradientStartPoint: { x: 0, y: 0 },
+        fillRadialGradientStartRadius: 0,
+        fillRadialGradientEndPoint: { x: 0, y: 0 },
+        fillRadialGradientEndRadius: 30,
+        fillRadialGradientColorStops: [0, 'white', 1, 'red'],
+        stroke: 'white',
+        strokeWidth: 2,
+        lineJoin: 'round',
+        angle: 1,
+        radius: 30,
+        x: stage.width() / 2,
+        y: 30,
+        rotation: -90,
+        shadowColor: 'black',
+        shadowOffsetX: 3,
+        shadowOffsetY: 3,
+        shadowBlur: 2,
+        shadowOpacity: 0.5,
+      })
+
+      layer.add(wheel)
+      layer.add(pointer)
+      stage.add(layer)
+
+      wheel.on('mousedown touchstart', function (evt) {
+        angularVelocity = 0
+        controlled = true
+        target = evt.target as Wedge
+        finished = false
+      })
+      stage.addEventListener('mousemove touchmove', function () {
+        var mousePos = stage.getPointerPosition()
+        if (controlled && mousePos && target) {
+          var x = mousePos.x - wheel.x()
+          var y = mousePos.y - wheel.y()
+          var atan = Math.atan(y / x)
+          var rotation = x >= 0 ? atan : atan + Math.PI
+          var targetGroup = target.getParent()
+
+          wheel.rotation(
+            rotation - parseFloat(targetGroup.id()) - target.angle() / 2
+          )
+        }
+      })
+      stage.addEventListener('mouseup touchend', function () {
+        controlled = false
+        angularVelocity = handleAverageAngularVelocity() * 5
+
+        if (angularVelocity > 20) {
+          angularVelocity = 20
+        } else if (angularVelocity < -20) {
+          angularVelocity = -20
+        }
+        angularVelocities.forEach(() => {
+          angularVelocities.shift()
+        })
+      })
+
+      anim = new Konva.Animation(animate, layer)
+    }
+    const handleStart = () => {
+      let i = 5
+      while (i) {
+        let j = Math.floor(Math.random() * i--)
+        ;[six[j], six[i]] = [six[i], six[j]]
+      }
+      finished = false
+      angularVelocity = 15
+      result.value=''
+      ;(stage.getChildren()[0].getChildren()[0] as Group)
+        .getChildren()
+        .forEach((c, i) => {
+          ;((c as Group).getChildren()[1] as Text).text(six[i])
+        })
+      console.log(stage)
+      anim?.start()
+    }
+
+    onMounted(() => {
+      init()
+    })
+    return {
+      handleStart,
+      result,
+    }
+  },
+})
+</script>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
